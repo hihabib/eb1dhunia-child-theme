7d3b801338717ca4ae8c7e72f1285968f6e73bc1
Habibul Islam <habibulislam6862@gmail.com>
2024-08-31 20:21:36 +0600

    updated

--- functions.php
+++ functions.php
@@ -15,21 +15,114 @@ function hello_elementor_child_enqueue_styles()
 }
 
 // add internal css and js
-add_action('wp_head', function (){
-    if(is_product()){
+add_action('wp_head', function () {
+    if (is_product()) {
         ob_start();
         ?>
-        <style>/*! tabbyjs v12.0.3 | (c) 2019 Chris Ferdinandi | MIT License | http://github.com/cferdinandi/tabby */
-            [role=tablist]{border-bottom:1px solid #d3d3d3;list-style:none;margin:0;padding:0}[role=tablist] *{box-sizing:border-box}@media (min-width:30em){[role=tablist] li{display:inline-block}}[role=tab]{border:1px solid transparent;border-top-color:#d3d3d3;display:block;padding:.5em 1em;text-decoration:none}@media (min-width:30em){[role=tab]{border-top-color:transparent;border-top-left-radius:.5em;border-top-right-radius:.5em;display:inline-block;margin-bottom:-1px}}[role=tab][aria-selected=true]{background-color:#d3d3d3}@media (min-width:30em){[role=tab][aria-selected=true]{background-color:transparent;border:1px solid #d3d3d3;border-bottom-color:#fff}}[role=tab]:hover:not([aria-selected=true]){background-color:#f7f7f7}@media (min-width:30em){[role=tab]:hover:not([aria-selected=true]){border:1px solid #d3d3d3}}[hidden]{display:none}</style>
+        <style>.js-tabs {
+                margin: 2em;
+                max-width: 100%
+            }
+
+            .js-tabs__header {
+                display: block;
+                margin: 0;
+                padding: 0;
+                overflow: hidden
+            }
+
+            .js-tabs__header li {
+                display: inline-block;
+                float: left
+            }
+
+            .js-tabs__title {
+                background: #f5f5f5;
+                border: 1px solid #ccc;
+                cursor: pointer;
+                display: block;
+                margin-right: .5em;
+                padding: 1em 1.5em;
+                transition: .25s
+            }
+
+            .js-tabs__title:hover {
+                text-decoration: none
+            }
+
+            .js-tabs__title-active {
+                background: #fff;
+                border-bottom-color: #fff;
+                border-top-left-radius: .75em
+            }
+
+            .js-tabs__content {
+                border: 1px solid #ccc;
+                line-height: 1.5;
+                margin-top: -1px;
+                padding: 1em 2em 3em
+            }</style>
         <?php echo ob_get_clean();
     }
 });
 
-add_action('wp_footer', function (){
-    if(is_product()){
+add_action('wp_footer', function () {
+    if (is_product()) {
         ob_start(); ?>
-        <script>/*! tabbyjs v12.0.3 | (c) 2019 Chris Ferdinandi | MIT License | http://github.com/cferdinandi/tabby */
-            Element.prototype.matches||(Element.prototype.matches=Element.prototype.msMatchesSelector||Element.prototype.webkitMatchesSelector),Element.prototype.closest||(Element.prototype.matches||(Element.prototype.matches=Element.prototype.msMatchesSelector||Element.prototype.webkitMatchesSelector),Element.prototype.closest=function(e){var t=this;if(!document.documentElement.contains(this))return null;do{if(t.matches(e))return t;t=t.parentElement}while(null!==t);return null}),(function(e,t){"function"==typeof define&&define.amd?define([],(function(){return t(e)})):"object"==typeof exports?module.exports=t(e):e.Tabby=t(e)})("undefined"!=typeof global?global:"undefined"!=typeof window?window:this,(function(e){"use strict";var t={idPrefix:"tabby-toggle_",default:"[data-tabby-default]"},r=function(t){if(t&&"true"!=t.getAttribute("aria-selected")){var r=document.querySelector(t.hash);if(r){var o=(function(e){var t=e.closest('[role="tablist"]');if(!t)return{};var r=t.querySelector('[role="tab"][aria-selected="true"]');if(!r)return{};var o=document.querySelector(r.hash);return r.setAttribute("aria-selected","false"),r.setAttribute("tabindex","-1"),o?(o.setAttribute("hidden","hidden"),{previousTab:r,previousContent:o}):{previousTab:r}})(t);!(function(e,t){e.setAttribute("aria-selected","true"),e.setAttribute("tabindex","0"),t.removeAttribute("hidden"),e.focus()})(t,r),o.tab=t,o.content=r,(function(t,r){var o;"function"==typeof e.CustomEvent?o=new CustomEvent("tabby",{bubbles:!0,cancelable:!0,detail:r}):(o=document.createEvent("CustomEvent")).initCustomEvent("tabby",!0,!0,r),t.dispatchEvent(o)})(t,o)}}},o=function(e,t){var o=(function(e){var t=e.closest('[role="tablist"]'),r=t?t.querySelectorAll('[role="tab"]'):null;if(r)return{tabs:r,index:Array.prototype.indexOf.call(r,e)}})(e);if(o){var n,i=o.tabs.length-1;["ArrowUp","ArrowLeft","Up","Left"].indexOf(t)>-1?n=o.index<1?i:o.index-1:["ArrowDown","ArrowRight","Down","Right"].indexOf(t)>-1?n=o.index===i?0:o.index+1:"Home"===t?n=0:"End"===t&&(n=i),r(o.tabs[n])}};return function(n,i){var a,l,u={};u.destroy=function(){var e=l.querySelectorAll("a");Array.prototype.forEach.call(e,(function(e){var t=document.querySelector(e.hash);t&&(function(e,t,r){e.id.slice(0,r.idPrefix.length)===r.idPrefix&&(e.id=""),e.removeAttribute("role"),e.removeAttribute("aria-controls"),e.removeAttribute("aria-selected"),e.removeAttribute("tabindex"),e.closest("li").removeAttribute("role"),t.removeAttribute("role"),t.removeAttribute("aria-labelledby"),t.removeAttribute("hidden")})(e,t,a)})),l.removeAttribute("role"),document.documentElement.removeEventListener("click",c,!0),l.removeEventListener("keydown",s,!0),a=null,l=null},u.setup=function(){if(l=document.querySelector(n)){var e=l.querySelectorAll("a");l.setAttribute("role","tablist"),Array.prototype.forEach.call(e,(function(e){var t=document.querySelector(e.hash);t&&(function(e,t,r){e.id||(e.id=r.idPrefix+t.id),e.setAttribute("role","tab"),e.setAttribute("aria-controls",t.id),e.closest("li").setAttribute("role","presentation"),t.setAttribute("role","tabpanel"),t.setAttribute("aria-labelledby",e.id),e.matches(r.default)?e.setAttribute("aria-selected","true"):(e.setAttribute("aria-selected","false"),e.setAttribute("tabindex","-1"),t.setAttribute("hidden","hidden"))})(e,t,a)}))}},u.toggle=function(e){var t=e;"string"==typeof e&&(t=document.querySelector(n+' [role="tab"][href*="'+e+'"]')),r(t)};var c=function(e){var t=e.target.closest(n+' [role="tab"]');t&&(e.preventDefault(),r(t))},s=function(e){var t=document.activeElement;t.matches(n+' [role="tab"]')&&(["ArrowUp","ArrowDown","ArrowLeft","ArrowRight","Up","Down","Left","Right","Home","End"].indexOf(e.key)<0||o(t,e.key))};return a=(function(){var e={};return Array.prototype.forEach.call(arguments,(function(t){for(var r in t){if(!t.hasOwnProperty(r))return;e[r]=t[r]}})),e})(t,i||{}),u.setup(),(function(t){if(!(e.location.hash.length<1)){var o=document.querySelector(t+' [role="tab"][href*="'+e.location.hash+'"]');r(o)}})(n),document.documentElement.addEventListener("click",c,!0),l.addEventListener("keydown",s,!0),u}}));</script>
+        <script>/**
+             * Vanilla JavaScript Tabs v2.0.1
+             * https://zoltantothcom.github.io/vanilla-js-tabs
+             */
+            const Tabs = function (e) {
+                    var t = document.getElementById(e.elem);
+                    if (!t) throw new Error(`Element with ID "${e.elem}" not found`);
+                    const n = t;
+                    let l = e.open || 0;
+                    const r = "js-tabs__title", c = "js-tabs__title-active", o = "js-tabs__content",
+                        a = n.querySelectorAll("." + r).length;
+
+                    function s(e) {
+                        n.addEventListener("click", i);
+                        var t = d(null == e ? l : e);
+                        for (let e = 0; e < a; e++) n.querySelectorAll("." + r)[e].setAttribute("data-index", e.toString()), e === t && f(e)
+                    }
+
+                    function i(e) {
+                        var t = e.target.closest("." + r);
+                        t && (e.preventDefault(), f(parseInt(null != (e = t.getAttribute("data-index")) ? e : "0")))
+                    }
+
+                    function u() {
+                        [].forEach.call(n.querySelectorAll("." + o), e => {
+                            e.style.display = "none"
+                        }), [].forEach.call(n.querySelectorAll("." + r), e => {
+                            e.className = function (e, t) {
+                                t = new RegExp(`(\\s|^)${t}(\\s|$)`, "g");
+                                return e.replace(t, "")
+                            }(e.className, c)
+                        })
+                    }
+
+                    function d(e) {
+                        return e < 0 || isNaN(e) || e >= a ? 0 : e
+                    }
+
+                    function f(e) {
+                        u();
+                        e = d(e);
+                        n.querySelectorAll("." + r)[e].classList.add(c), n.querySelectorAll("." + o)[e].style.display = ""
+                    }
+
+                    function y() {
+                        n.removeEventListener("click", i)
+                    }
+
+                    return s(), {
+                        open: f, update: function (e) {
+                            y(), u(), s(e)
+                        }, destroy: y
+                    }
+                };</script>
         <?php echo ob_get_clean();
     }
 });
@@ -66,23 +159,23 @@ function wdm_empty_cart($cart_item_data, $product_id, $variation_id)
 add_shortcode("pricing_tabs", function () {
     ob_start();
     ?>
-    <ul data-tabs>
-        <?php
-        global $product;
-        $package_terms = $product->get_attributes()['pa_service-type']->get_terms();
-        foreach ($package_terms as $index => $package_term) :
-
-            ?>
-            <li><a <?php echo $index === 0 ? "data-tabby-default" : ""; ?>
-                        href="#<?php echo $package_term -> slug; ?>"><?php echo $package_term->name; ?>></a></li>
+    <div class="js-tabs" id="tabs">
+        <ul class="js-tabs__header">
+            <?php
+            global $product;
+            $package_terms = $product->get_attributes()['pa_service-type']->get_terms();
+            foreach ($package_terms as $package_term) :
+                ?>
+                <li><a class="js-tabs__title" href="#"><?php echo $package_term->name; ?></a></li>
+            <?php endforeach; ?>
+        </ul>
+        <?php foreach ($package_terms as $package_term) : ?>
+            <div class="js-tabs__content">
+                <?php echo $package_term->description; ?>
+            </div>
         <?php endforeach; ?>
-    </ul>
-
-    <?php foreach ($package_terms as $package_term) : ?>
-    <div id="<?php echo $package_term -> slug ?>">
-        <?php echo $package_term -> description; ?>
     </div>
-    <?php endforeach; ?>
+
     <?php
     return ob_get_clean();
 });
