43a072286a1b6d76f6a078f55602b6c25dfa55ee
Habibul Islam <habibulislam6862@gmail.com>
2024-08-31 20:47:51 +0600

    updated

--- functions.php
+++ functions.php
@@ -107,17 +107,22 @@ add_shortcode("pricing_tabs", function () {
         <ul class="js-tabs__header">
             <?php
             global $product;
-            $package_terms = $product->get_attributes()['pa_service-type']->get_terms();
-            foreach ($package_terms as $package_term) :
+            $unsorted_terms = $product->get_attributes()['pa_service-type']->get_terms();
+            $sorted_terms = [$unsorted_terms[0], $unsorted_terms[2], $unsorted_terms[1]];
+            foreach ($sorted_terms as $package_term) :
                 ?>
                 <li><a class="js-tabs__title" href="#"><?php echo $package_term->name; ?></a></li>
             <?php endforeach; ?>
         </ul>
-        <?php foreach ($package_terms as $package_term) : ?>
+        <?php foreach ($sorted_terms as $package_term) : ?>
             <div class="js-tabs__content">
                 <?php echo $package_term->description; ?>
             </div>
-        <?php endforeach; ?>
+        <?php endforeach;
+        unset($unsorted_terms);
+        unset($sorted_terms);
+        unset($package_term);
+        ?>
     </div>
 
     <?php
